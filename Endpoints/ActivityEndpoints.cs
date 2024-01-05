befaedd4579d5985dea5552b58838197ed1d4448
Lukasz Momot <lukasz.momot@hotmail.com>
2024-01-05 14:22:40 +0100

    Add get activity by id endpoint

--- Endpoints/ActivityEndpoints.cs
+++ Endpoints/ActivityEndpoints.cs
@@ -12,12 +12,21 @@ public static class ActivityEndpoints
         var group = app.MapGroup("api/activities");
         
         group.MapGet("", GetAllActivities);
+        group.MapGet("{id:int}", GetActivityById);
     }
 
     private static async Task<Ok<List<Activity>>> GetAllActivities(BoredDbContext dbContext)
     {
         var activities = await dbContext.Activities.ToListAsync();
         var res = TypedResults.Ok(activities);
+
+        return res;
+    }
+    
+    private static async Task<Ok<Activity>> GetActivityById(BoredDbContext dbContext, int id)
+    {
+        var activity = await dbContext.Activities.FindAsync(id);
+        var res = TypedResults.Ok(activity);
 
         return res;
     }

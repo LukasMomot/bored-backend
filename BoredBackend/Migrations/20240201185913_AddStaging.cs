f91ad672adb5a3bf773402b3c6c6bd2f57143ddb
Lukasz Momot <lukasz.momot@hotmail.com>
2024-02-01 20:00:48 +0100

    Add staging migration

---
+++ BoredBackend/Migrations/20240201185913_AddStaging.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoredBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStaging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActivityStaging",
+                schema: "Bored",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Activity = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Participants = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    Accessibility = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityStaging", x => x.Key);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityStaging",
+                schema: "Bored");
+        }
+    }
+}

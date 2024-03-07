12a37b5bf24b07ce3e2038bd3f4d553804a23cbc
Lukasz Momot <lukasz.momot@hotmail.com>
2024-03-07 21:17:53 +0100

    change endpoint

--- Bored.CloudFunctions/GetStagedActivities.cs
+++ Bored.CloudFunctions/GetStagedActivities.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Net;
 using BoredBackend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.EntityFrameworkCore;
@@ -13,16 +15,15 @@ public class GetStagedActivities(ILoggerFactory loggerFactory, BoredDbContext db
     private readonly ILogger _logger = loggerFactory.CreateLogger<GetStagedActivities>();
 
     [Function(nameof(GetStagedActivities))]
-    public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
         FunctionContext executionContext)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request");
 
         // Check if take available
         var activities = dbContext.StagedActivities.AsNoTracking();
-        
-        var takeQueryParam = req.Query.Get("take");
+        req.Query.TryGetValue("take", out var takeQueryParam);
         if(int.TryParse(takeQueryParam, out var take))
         {
             activities = activities.Take(take);
@@ -32,10 +33,7 @@ public class GetStagedActivities(ILoggerFactory loggerFactory, BoredDbContext db
         
         _logger.LogInformation("Staged activities count: {count}", activitiesResult.Count);
         
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        await response.WriteAsJsonAsync(activities);
-        
-        return response;
+        return new OkObjectResult(activitiesResult);
         
     }
 }

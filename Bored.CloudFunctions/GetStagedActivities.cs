25d7c424491dde0991904340a395987f1c6006db
Lukasz Momot <lukasz.momot@hotmail.com>
2024-03-23 21:31:40 +0100

    Add open api attributes

--- Bored.CloudFunctions/GetStagedActivities.cs
+++ Bored.CloudFunctions/GetStagedActivities.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.Net;
+using Bored.Models;
 using BoredBackend.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Bored.CloudFunctions;
 
@@ -15,6 +18,9 @@ public class GetStagedActivities(ILoggerFactory loggerFactory, BoredDbContext db
     private readonly ILogger _logger = loggerFactory.CreateLogger<GetStagedActivities>();
 
     [Function(nameof(GetStagedActivities))]
+    [OpenApiOperation(operationId: "GetStagedActivities", Summary = "Get staged activities", Description = "This endpoint returns staged activities.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<ActivityStaging>), Description = "This returns the response")]
+    [OpenApiParameter(name: "take", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "Number of activities to take")]
     public IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
         FunctionContext executionContext)

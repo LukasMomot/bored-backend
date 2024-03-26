3739a0b412d668cd4057ad4070c3da2070373587
Lukasz Momot <lukasz.momot@hotmail.com>
2024-03-26 09:06:27 +0100

    Add ServiceBus lib

---
+++ Bored.Models/Messages/StartFakeStaging.cs
@@ -0,0 +1,8 @@
+namespace Bored.Models.Messages;
+
+// TODO: check record
+public class StartFakeStaging()
+{
+    public int ProcessId { get; set; }
+    public bool StartImmediately { get; set; }
+}
--- BoredBackend/Endpoints/ActivityEndpoints.cs
+++ BoredBackend/Endpoints/ActivityEndpoints.cs
@@ -1,7 +1,11 @@
 
+using System.Text.Json;
+using Azure.Messaging.ServiceBus;
 using Bored.Models;
+using Bored.Models.Messages;
 using BoredBackend.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BoredBackend.Endpoints;
@@ -13,8 +17,23 @@ public static class ActivityEndpoints
         var group = app.MapGroup("api/activities");
         
         group.MapGet("", GetAllActivities);
-        
         group.MapGet("{id:int}", GetActivityById);
+        group.MapPost("fakeStage", FakeStageActivity);
+        
+    }
+
+    private static async Task FakeStageActivity([FromServices] IConfiguration configuration)
+    {
+        var connectionString = configuration["ServiceBus"];
+        await using var client = new ServiceBusClient(connectionString);
+
+        await using ServiceBusSender sender = client.CreateSender("start-fake-staging");
+        
+        var msg = new StartFakeStaging{ ProcessId = 1, StartImmediately = true };
+        var json = JsonSerializer.Serialize(msg);
+        var sbMsg = new ServiceBusMessage(json);
+
+        await sender.SendMessageAsync(sbMsg);
     }
 
     private static async Task<Ok<List<Activity>>> GetAllActivities(BoredDbContext dbContext)
